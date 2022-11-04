3665a4a56c395c7483ce40304a2fe3bb847412dc
schan610 <s.chan610@gmail.com>
2022-11-04 15:46:29 -0700

    fixed unsecure API url

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,4 @@
 // This script contains reusable variables
 export const API_KEY = "2256b03b0968676221d29f819be2b449";
-
-export const API_GEOCODE_URL = `http://api.openweathermap.org/geo/1.0/direct?`;
+export const API_GEOCODE_URL = `https://api.openweathermap.org/geo/1.0/direct?`;
 export const API_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?`;
